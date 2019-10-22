c5505560376180855cda667186aca7fab04e9a67
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2019-10-22 08:21:20 +0200

    Clean up some more

--- src/Tree.stories.tsx
+++ src/Tree.stories.tsx
@@ -4,8 +4,45 @@ import { storiesOf } from '@storybook/react';
 import React, { useCallback, useState } from 'react';
 
 import { useTree } from 'index';
-import { testSource } from 'test/testsource';
-import { StatefulTreeNode, TreeState } from 'types/tree';
+import { StatefulTreeNode, TreeState } from 'types';
+
+// Generate strings 'a' through 'z'.
+function letterRange(): string[] {
+    return range(('a').charCodeAt(0), ('z').charCodeAt(0)).map((x) => String.fromCharCode(x));
+}
+
+// Generate an inclusive range.
+function range(start: number, end: number): number[] {
+    return [...Array(end - start + 1)].map((_, i) => i + start);
+}
+
+async function timeout(ms) {
+    return new Promise((resolve) => { setTimeout(resolve, ms); });
+}
+
+const testSource = {
+    async children(id?: string | null | undefined) {
+        console.log('source load children', id);
+        const parentId = id || '';
+        await timeout(100);
+        return letterRange().map((x) => ({
+            id: parentId + x,
+            label: parentId + x,
+            hasChildren: true,
+        }));
+    },
+    async trail(id: string) {
+        console.log('source load trail', id);
+        await timeout(100);
+        return range(1, id.length).reverse().map((length) => {
+            return {
+                id: id.substr(0, length),
+                label: id.substr(0, length),
+                hasChildren: true,
+            };
+        });
+    },
+};
 
 interface Labeled {
     label: string;
@@ -13,7 +50,6 @@ interface Labeled {
 
 const stories = storiesOf('Tree', module);
 
-const src = testSource();
 const renderAction = action('render');
 
 interface IListProps {
@@ -65,7 +101,7 @@ const emptyTreeState: TreeState = {};
 
 const TreeContainer: React.FC<{ activeId?: string }> = ({ activeId }) => {
     const [treeState, setTreeState] = useState(emptyTreeState);
-    const tree = useTree(src, treeState);
+    const tree = useTree(testSource, treeState);
     if (treeState.activeId !== activeId) {
         setTreeState((st) => ({ ...st, activeId}));
     }
